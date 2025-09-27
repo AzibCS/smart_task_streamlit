663ee9bab53447c2875587c86e85c8a7aec68ff4
AzibCS <djbravo0315@gmail.com>
2025-09-28 03:42:22 +0500

    updated google_auth

--- services/google_auth.py
+++ services/google_auth.py
@@ -1,11 +1,9 @@
-import os
-import pickle
 import streamlit as st
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-# If modifying these SCOPES, delete the file token.pickle.
+# If modifying these SCOPES, delete the token in session_state
 SCOPES = [
     "https://www.googleapis.com/auth/calendar.readonly",
     "https://www.googleapis.com/auth/gmail.readonly"
@@ -17,12 +15,11 @@ def get_google_credentials():
     # Path inside secrets (string JSON)
     client_secrets_json = st.secrets["google"]["client_secrets"]
 
-    # Load flow type from secrets
-    oauth_flow = st.secrets["google"].get("oauth_flow", "local")
-
     # Try to load token from session_state
     if "google_token" in st.session_state:
-        creds = Credentials.from_authorized_user_info(st.session_state["google_token"], SCOPES)
+        creds = Credentials.from_authorized_user_info(
+            st.session_state["google_token"], SCOPES
+        )
 
     # If no valid creds available, let user log in.
     if not creds or not creds.valid:
@@ -34,10 +31,8 @@ def get_google_credentials():
                 SCOPES
             )
 
-            if oauth_flow == "console":
-                creds = flow.run_console()
-            else:
-                creds = flow.run_local_server(port=0)
+            # ✅ Always use local server (console removed in latest versions)
+            creds = flow.run_local_server(port=0)
 
         # Save the credentials to session_state
         st.session_state["google_token"] = creds.to_json()
